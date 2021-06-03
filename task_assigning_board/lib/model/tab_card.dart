22459213b0da94450b750a84713114049a77c6da
NewTHESimper <newjanpi1540@gmail.com>
2021-06-04 00:57:04 +0700

    [ADD] TAB home (Not complete)

--- task_assigning_board/lib/model/tab_card.dart
+++ task_assigning_board/lib/model/tab_card.dart
@@ -2,120 +2,133 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-import '../size_config.dart';
+class TabCard extends StatelessWidget {
+  final DocumentSnapshot index;
 
-class RecipeCard extends StatelessWidget {
-  final DocumentReference recipeDetail;
-  final Color color;
-
-  const RecipeCard({Key key, this.recipeDetail, this.color}) : super(key: key);
+  const TabCard({Key key, this.index}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    double defaultSize = SizeConfig.defaultSize;
-
     return StreamBuilder(
-      stream: recipeDetail.snapshots(),
+      stream: FirebaseFirestore.instance
+          .collection("Task")
+          .doc(index.id)
+          .snapshots(),
       builder:
           (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
         if (!snapshot.hasData) {
           return Text('Loading...');
         }
-        var repCard = snapshot.data.data();
+        var task = snapshot.data.data();
         return InkWell(
-          // onTap: () {
-          //   Navigator.push(
-          //       context,
-          //       MaterialPageRoute(
-          //           builder: (context) => RecipeScreen(
-          //                 recipeIndex: snapshot.data,
-          //               )));
-          // },
+          onTap: () {
+            //   Navigator.push(
+            //       context,
+            //       MaterialPageRoute(
+            //           builder: (context) => RecipeScreen(
+            //                 recipeIndex: snapshot.data,
+            //               )));
+          },
           child: Stack(
             children: [
               Padding(
-                padding: EdgeInsets.fromLTRB(6.5, 3, 0, 0),
+                padding: EdgeInsets.all(5),
                 child: Container(
-                  height: defaultSize * 100,
-                  width: defaultSize * 17,
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  width: MediaQuery.of(context).size.width * 0.5,
                   child: Card(
-                    color: color,
                     shape: RoundedRectangleBorder(
-                      borderRadius:
-                          BorderRadius.circular(defaultSize * 1.5), // 15
+                      borderRadius: BorderRadius.circular(
+                          MediaQuery.of(context).size.height * (0.005)), // 15
                     ),
-                    elevation: 8,
+                    elevation: 15,
                     child: Container(
                       child: Row(
                         children: [
                           Expanded(
                             child: Padding(
-                              padding: EdgeInsets.all(defaultSize),
+                              padding: EdgeInsets.all(8),
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  // Recipe Time
-                                  Padding(
-                                    padding: EdgeInsets.fromLTRB(8, 0, 0, 3),
-                                    child: SvgPicture.asset(
-                                      "assets/icons/clock.svg",
-                                      height: defaultSize * 2.5,
-                                      width: defaultSize * 2.5,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.fromLTRB(3, 0, 0, 0),
-                                    child: Text(
-                                      "${repCard['time'].toString()} M",
-                                      style: TextStyle(
-                                        fontSize: defaultSize * 1.8, //20
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                  ),
                                   Spacer(),
-
-                                  //Recipe Name
+                                  //Task Name
                                   Padding(
                                     padding: EdgeInsets.fromLTRB(5, 0, 0, 8),
                                     child: Text(
-                                      repCard['name'],
+                                      task["ProjectName"],
                                       style: TextStyle(
-                                        fontSize: defaultSize * 1.8,
-                                        color: Colors.white,
+                                        fontSize: 15,
+                                        color: Colors.black,
                                       ),
                                       maxLines: 2,
                                       overflow: TextOverflow.ellipsis,
                                     ),
                                   ),
+
+                                  //Task Description
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(5, 0, 0, 8),
+                                    child: Text(
+                                      task["ShortDesc"],
+                                      style: TextStyle(
+                                        fontSize: 10,
+                                        color: Colors.black,
+                                      ),
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                  Spacer(),
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 5, horizontal: 20),
+                                    child: Container(
+                                      color: Colors.deepPurpleAccent,
+                                      height: 1,
+                                      width: 120,
+                                    ),
+                                  ),
+
+                                  //Client
+                                  Padding(
+                                    padding: EdgeInsets.all(8),
+                                    child: Text(
+                                      task["Company"],
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ),
+                                  Spacer(),
                                 ],
                               ),
                             ),
                           ),
-                          SizedBox(width: defaultSize * 0.5),
+                          SizedBox(width: 2.4 * 0.5),
                         ],
                       ),
                     ),
                   ),
                 ),
               ),
-              Positioned(
-                left: 62,
-                top: 0,
-                child: Container(
-                  child: CircleAvatar(
-                    backgroundImage: NetworkImage(repCard['imgUrl']),
-                    backgroundColor: Colors.transparent,
-                    radius: 58.0,
-                  ),
-                  decoration: new BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: new Border.all(
-                        // color: kPrimaryColor,
-                        width: 4.5,
-                      )),
-                ),
-              ),
+              // Positioned(
+              //   left: 62,
+              //   top: 0,
+              //   child: Container(
+              //     child: CircleAvatar(
+              //       backgroundImage: NetworkImage("image"),
+              //       backgroundColor: Colors.transparent,
+              //       radius: 58.0,
+              //     ),
+              //     decoration: new BoxDecoration(
+              //         shape: BoxShape.circle,
+              //         border: new Border.all(
+              //           // color: kPrimaryColor,
+              //           width: 4.5,
+              //         )),
+              //   ),
+              // ),
             ],
           ),
         );
